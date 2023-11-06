bf9e5cc7da3cecb8d0b4b59edfd8476af87233fb
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2023-11-06 14:15:01 -0500

    create a login component

---
+++ components/login/login.jsx
@@ -0,0 +1,67 @@
+// components/Login.jsx
+import React, { useState } from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({ username: '', password: '' });
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/login', credentials);
+      // Handle response, such as saving the token, navigating to another page, etc.
+      console.log(response.data);
+    } catch (error) {
+      console.error('Login failed:', error);
+      // Handle error, such as showing a notification to the user
+    }
+  };
+
+  return (
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="username"
+        label="Username"
+        name="username"
+        autoComplete="username"
+        autoFocus
+        value={credentials.username}
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={credentials.password}
+        onChange={handleChange}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign In
+      </Button>
+    </Box>
+  );
+};
+
+export default Login;
