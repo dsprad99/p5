3690afa8e7991e0eb492dedd914f0e385d311090
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2023-11-06 14:12:36 -0500

    Handle userlogin

--- photoShare.jsx
+++ photoShare.jsx
@@ -1,82 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  HashRouter, Route, Switch
-} from 'react-router-dom';
-import {
-  Grid, Paper
-} from '@mui/material';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Grid, Paper } from '@mui/material';
 import './styles/main.css';
-
-// import necessary components
-import TopBar from './components/topBar/TopBar';
-import UserDetail from './components/userDetail/userDetail';
-import UserList from './components/userList/userList';
-import UserPhotos from './components/userPhotos/userPhotos';
-import UserComments from './components/userComments/userComments';
-import SinglePhoto from './components/singlePhoto/singlePhoto';
 
 class PhotoShare extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       main_content: undefined,
-      advanced_features: false
+      advanced_features: false,
+      user: null // This will store the user's information or null if not logged in
     };
     this.changeMainContent = this.changeMainContent.bind(this);
     this.toggleAdvancedFeatures = this.toggleAdvancedFeatures.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
-  changeMainContent = (main_content) => {
-    this.setState({ main_content: main_content });
-  };
+  changeMainContent(main_content) {
+    this.setState({ main_content });
+  }
 
   toggleAdvancedFeatures() {
-    this.setState({
-      advanced_features: !this.state.advanced_features
-    });
+    this.setState(prevState => ({
+      advanced_features: !prevState.advanced_features
+    }));
+  }
+
+  handleLogin(userData) {
+    this.setState({ user: userData });
+  }
+
+  handleLogout() {
+    this.setState({ user: null });
   }
 
   render() {
     return (
       <HashRouter>
-      <div>
-      <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <TopBar main_content={this.state.main_content} toggle_feature={this.toggleAdvancedFeatures}/>
-        </Grid>
-        <div className="main-topbar-buffer"/>
-        <Grid item sm={3}>
-          <Paper className="main-grid-item">
-            <UserList advanced_features={this.state.advanced_features}/>
-          </Paper>
-        </Grid>
-        <Grid item sm={9}>
-          <Paper className="main-grid-item">
-            <Switch>
-              <Route path="/users/:userId"
-                     render={ props => <UserDetail {...props} changeMainContent={this.changeMainContent}/> }
+        <div>
+          <Grid container spacing={8}>
+            <Grid item xs={12}>
+              <TopBar
+                user={this.state.user}
+                onLogout={this.handleLogout}
+                main_content={this.state.main_content}
+                toggle_feature={this.toggleAdvancedFeatures}
               />
-              <Route path="/photos/:userId"
-                     render ={ props => <UserPhotos {...props} changeMainContent={this.changeMainContent}/> }
-              />
-              <Route path="/comments/:userId"
-                     render ={ props => <UserComments {...props} changeMainContent={this.changeMainContent} advanced_features={this.state.advanced_features}/> }
-              />
-              <Route path="/single-photo/:photoId"
-                     render ={ props => <SinglePhoto {...props} changeMainContent={this.changeMainContent} advanced_features={this.state.advanced_features}/> }
-              />
-            </Switch>
-          </Paper>
-        </Grid>
-      </Grid>
-      </div>
+            </Grid>
+            <div className="main-topbar-buffer" />
+            <Grid item sm={3}>
+              {this.state.user && (
+                <Paper className="main-grid-item">
+                  <UserList advanced_features={this.state.advanced_features} />
+                </Paper>
+              )}
+            </Grid>
+            <Grid item sm={9}>
+              <Paper className="main-grid-item">
+                <Switch>
+                  <Route path="/users/:userId"
+                    render={props => <UserDetail {...props} changeMainContent={this.changeMainContent} />}
+                  />
+                  <Route path="/photos/:userId"
+                    render={props => <UserPhotos {...props} changeMainContent={this.changeMainContent} />}
+                  />
+                  <Route path="/comments/:userId"
+                    render={props => <UserComments {...props} changeMainContent={this.changeMainContent} advanced_features={this.state.advanced_features} />}
+                  />
+                  <Route path="/single-photo/:photoId"
+                    render={props => <SinglePhoto {...props} changeMainContent={this.changeMainContent} advanced_features={this.state.advanced_features} />}
+                  />
+                </Switch>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </HashRouter>
     );
   }
 }
 
 ReactDOM.render(
-  <PhotoShare/>,
+  <PhotoShare />,
   document.getElementById('photoshareapp')
 );
