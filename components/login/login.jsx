465b6de54da207a6c915a3d6465ac3c9d74686af
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2023-11-07 01:53:02 -0500

    Update login.jsx

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -1,54 +1,65 @@
-// components/Login.jsx
 import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography, Snackbar, Alert } from '@mui/material';
 import axios from 'axios';
 
-const Login = (props) => {
+const LoginRegister = (props) => {
+   {/*state hook to help manage credential as default will be empty*/ }
   const [credentials, setCredentials] = useState({ username: '', password: '' });
-  
+  {/*set the state for our error as false to start*/}
+  const [openSnackbar, setOpenSnackbar] = useState(false); 
+  const [error, setError] = useState(''); 
+
+  {/*event handler to update the state with new input*/}
   const handleChange = (event) => {
-    const { userName } = event.target;
-    setCredentials({ credentials, userName});
+    const { name, value } = event.target;
+    setCredentials({ ...credentials, [name]: value });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-      try {
-        //pass in our credentials to the post request
-      const response = await axios.post('/admin/login/', credentials);
-      //handle response
-      console.log(response.data);
-    } catch (error) {
-      console.error('Login failed:', error);
+    try {
+      {/*send post request to try and log in*/}
+      const response = await axios.post('/admin/login', {
+        username: credentials.username,
+      });
+      props.onLogin(response.data.user);
+      {/*opens the logged in users details up first*/}
+      props.history.push(`/users/${response.data.user._id}`);
     }
-    };
     
-    const handleLogout = async (event) => {
-        event.preventDefault();
-        try {
-            //pass in our credentials to the post request
-        const response = await axios.post('/admin/logout/', credentials);
-        //handle response
-        console.log(response.data);
-        } catch (error) {
-        console.error('Login failed:', error);
-        }
+    //if theres a catch then a username was incorrectly entered
+    catch (error) {
+      setError('Login failed. Please try again.'); 
+      {/*show our error*/}
+      setOpenSnackbar(true); 
+    }
+  };
+
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackbar(false);
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        flexDirection: 'column', 
+      }}>
       <Typography component="h1" variant="h5">
-        Sign in
+        Log In
       </Typography>
       <TextField
         margin="normal"
         required
         fullWidth
-        id="username"
         label="Username"
         name="username"
         autoComplete="username"
-        autoFocus
         value={credentials.username}
         onChange={handleChange}
       />
@@ -56,12 +67,27 @@ const Login = (props) => {
         type="submit"
         fullWidth
         variant="contained"
-        sx={{ mt: 3, mb: 2 }}
+        sx={{ mt: 3, mb: 3 }}
       >
-        Sign In
+        Log In
       </Button>
+      <Snackbar
+      open={openSnackbar}
+      onClose={handleCloseSnackbar}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+      <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '90%' }}>
+          {error}
+      </Alert>
+      </Snackbar>
     </Box>
   );
 };
 
-export default Login;
+export default LoginRegister;
+
+
+
+  
+  
+
