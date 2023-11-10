544981384d828ea1f29ba71b4671f9c27551df79
Cmichau11 <143210597+Cmichau11@users.noreply.github.com>
2023-11-10 02:22:31 -0500

    Update login.jsx problem4

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -4,7 +4,16 @@ import axios from 'axios';
 
 const LoginRegister = (props) => {
    {/*state hook to help manage credential as default will be empty*/ }
-  const [credentials, setCredentials] = useState({ username: '', password: '' });
+  const [credentials, setCredentials] = useState({
+      username: '',
+      password: '',
+      confirmPassword: '',
+      firstname: '',
+      lastname: '',
+      location: '',
+      description: '',
+      occupation: '',
+  });
   {/*set the state for our error as false to start*/}
   const [openSnackbar, setOpenSnackbar] = useState(false); 
   const [error, setError] = useState(''); 
@@ -21,6 +30,7 @@ const LoginRegister = (props) => {
       {/*send post request to try and log in*/}
       const response = await axios.post('/admin/login', {
         username: credentials.username,
+          password: credentials.password,
       });
       props.onLogin(response.data.user);
       {/*opens the logged in users details up first*/}
@@ -34,6 +44,37 @@ const LoginRegister = (props) => {
       setOpenSnackbar(true); 
     }
   };
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('/user', {
+                login_name: credentials.username,
+                password: credentials.password,
+                first_name: credentials.firstName,
+                last_name: credentials.lastName,
+                location: credentials.location,
+                description: credentials.description,
+                occupation: credentials.occupation,
+            });
+            // Handle successful registration, clear form fields, show success message
+            setCredentials({
+                username: '',
+                password: '',
+                confirmPassword: '',
+                firstName: '',
+                lastName: '',
+                location: '',
+                description: '',
+                occupation: '',
+            });
+            setError('Registration successful');
+            setOpenSnackbar(true);
+        } catch (error) {
+            setError('Registration failed. Please check your information.');
+            setOpenSnackbar(true);
+        }
+    };
 
   const handleCloseSnackbar = (event, reason) => {
     if (reason === 'clickaway') {
@@ -63,6 +104,19 @@ const LoginRegister = (props) => {
         value={credentials.username}
         onChange={handleChange}
       />
+
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            value={credentials.password}
+            onChange={handleChange}
+        />
+
       <Button
         type="submit"
         fullWidth
@@ -71,6 +125,89 @@ const LoginRegister = (props) => {
       >
         Log In
       </Button>
+
+        <Typography component="h1" variant="h5">
+            Register
+        </Typography>
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="First Name"
+            name="firstName"
+            autoComplete="first-name"
+            value={credentials.firstName}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Last Name"
+            name="lastName"
+            autoComplete="last-name"
+            value={credentials.lastName}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            fullWidth
+            label="Location"
+            name="location"
+            autoComplete="location"
+            value={credentials.location}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            fullWidth
+            label="Description"
+            name="description"
+            autoComplete="off"
+            value={credentials.description}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            fullWidth
+            label="Occupation"
+            name="occupation"
+            autoComplete="occupation-title"
+            value={credentials.occupation}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            value={credentials.password}
+            onChange={handleChange}
+        />
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Confirm Password"
+            name="confirmPassword"
+            type="password"
+            autoComplete="current-password"
+            value={credentials.confirmPassword}
+            onChange={handleChange}
+        />
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleRegister}
+        >
+            Register Me
+        </Button>
+
       <Snackbar
       open={openSnackbar}
       onClose={handleCloseSnackbar}
