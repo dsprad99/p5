7d187a7d77548f64c90235562ef8eff411175e7c
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2023-11-16 14:21:40 -0500

    login/register state bug fix

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -3,8 +3,8 @@ import { Box, Grid, Button, TextField, Typography, Snackbar, Alert } from '@mui/
 import axios from 'axios';
 
 function LoginRegister(props) {
-   /*state hook to help manage credential as default will be empty*/
-  const [credentials, setCredentials] = useState({
+  /*state hook to help manage credential as default will be empty*/
+  const [registerCredentials, setRegisterCredentials] = useState({
       username: '',
       password: '',
       confirmPassword: '',
@@ -14,23 +14,34 @@ function LoginRegister(props) {
       description: '',
       occupation: '',
   });
+
+  const [loginCredentials, setLoginCredentials] = useState({
+      loginUsername: '',
+      loginPassword: '',
+  });
   /*set the state for our error as false to start*/
   const [openSnackbar, setOpenSnackbar] = useState(false); 
   const [error, setError] = useState(''); 
 
-  /*event handler to update the state with new input*/
-  const handleChange = (event) => {
+  /*event handler to update the state with new input for login*/
+  const handleLoginChange = (event) => {
     const { name, value } = event.target;
-    setCredentials({ ...credentials, [name]: value });
+    setLoginCredentials({ ...loginCredentials, [name]: value });
   };
 
-  const handleSubmit = async (event) => {
+  /*event handler to update the state with new input for register*/
+  const handleRegisterChange = (event) => {
+    const { name, value } = event.target;
+    setRegisterCredentials({ ...registerCredentials, [name]: value });
+  };
+
+  const handleLogin = async (event) => {
     event.preventDefault();
     try {
       /*send post request to try and log in*/
       const response = await axios.post('/admin/login', {
-        username: credentials.username,
-          password: credentials.password,
+        username: loginCredentials.loginUsername,
+        password: loginCredentials.loginPassword,
       });
       props.onLogin(response.data.user);
       /*opens the logged in users details up first*/
@@ -48,23 +59,23 @@ function LoginRegister(props) {
     const handleRegister = async (event) => {
         event.preventDefault();
         // Check if passwords match
-        if (credentials.password !== credentials.confirmPassword) {
+        if (registerCredentials.password !== registerCredentials.confirmPassword) {
             setError('Passwords do not match');
             setOpenSnackbar(true);
             return;
         }
         try {
             await axios.post('/user', {
-                login_name: credentials.username,
-                password: credentials.password,
-                first_name: credentials.firstName,
-                last_name: credentials.lastName,
-                location: credentials.location,
-                description: credentials.description,
-                occupation: credentials.occupation,
+                login_name: registerCredentials.username,
+                password: registerCredentials.password,
+                first_name: registerCredentials.firstName,
+                last_name: registerCredentials.lastName,
+                location: registerCredentials.location,
+                description: registerCredentials.description,
+                occupation: registerCredentials.occupation,
             });
             // Handle successful registration, clear form fields, show success message
-            setCredentials({
+            setRegisterCredentials({
                 username: '',
                 password: '',
                 confirmPassword: '',
@@ -92,8 +103,8 @@ function LoginRegister(props) {
   };
 
   return (
-    <Grid component="form" container onSubmit={handleSubmit} noValidate spacing={3}>
-      <Grid component="div" item onSubmit={handleSubmit} noValidate xs>
+    <Grid component="form" container onSubmit={handleLogin} noValidate spacing={3}>
+      <Grid component="div" item onSubmit={handleLogin} noValidate xs>
         <Box component="div" noValidate sx={{
         display: 'flex',
         justifyContent: 'center',
@@ -107,33 +118,34 @@ function LoginRegister(props) {
           margin="normal"
           required
           label="Username"
-          name="username"
+          name="loginUsername"
           autoComplete="username"
-          value={credentials.username}
-          onChange={handleChange}
+          value={loginCredentials.loginUsername}
+          onChange={handleLoginChange}
           />
 
           <TextField
               margin="normal"
               required
               label="Password"
-              name="password"
+              name="loginPassword"
               type="password"
               autoComplete="current-password"
-              value={credentials.password}
-              onChange={handleChange}
+              value={loginCredentials.loginPassword}
+              onChange={handleLoginChange}
               />
 
         <Button
           type="submit"
           variant="contained"
           sx={{ mt: 3, mb: 3 }}
+          onChange={handleLogin}  
           >
           Log In
         </Button>
         </Box>
       </Grid>
-      <Grid component="div" item onSubmit={handleSubmit} noValidate xs>
+      <Grid component="div" item noValidate xs>
         <Box component="div" noValidate sx={{
         display: 'flex',
         justifyContent: 'center',
@@ -151,8 +163,8 @@ function LoginRegister(props) {
             label="First Name"
             name="firstName"
             autoComplete="first-name"
-            value={credentials.firstName}
-            onChange={handleChange}
+            value={registerCredentials.firstName}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
@@ -160,32 +172,32 @@ function LoginRegister(props) {
             label="Last Name"
             name="lastName"
             autoComplete="last-name"
-            value={credentials.lastName}
-            onChange={handleChange}
+            value={registerCredentials.lastName}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
             label="Location"
             name="location"
             autoComplete="location"
-            value={credentials.location}
-            onChange={handleChange}
+            value={registerCredentials.location}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
             label="Description"
             name="description"
             autoComplete="off"
-            value={credentials.description}
-            onChange={handleChange}
+            value={registerCredentials.description}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
             label="Occupation"
             name="occupation"
             autoComplete="occupation-title"
-            value={credentials.occupation}
-            onChange={handleChange}
+            value={registerCredentials.occupation}
+            onChange={handleRegisterChange}
             />
          <TextField
             margin="normal"
@@ -193,8 +205,8 @@ function LoginRegister(props) {
             label="Username"
             name="username"
             autoComplete="username"
-            value={credentials.username}
-            onChange={handleChange}
+            value={registerCredentials.username}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
@@ -203,8 +215,8 @@ function LoginRegister(props) {
             name="password"
             type="password"
             autoComplete="current-password"
-            value={credentials.password}
-            onChange={handleChange}
+            value={registerCredentials.password}
+            onChange={handleRegisterChange}
             />
         <TextField
             margin="normal"
@@ -213,8 +225,8 @@ function LoginRegister(props) {
             name="confirmPassword"
             type="password"
             autoComplete="current-password"
-            value={credentials.confirmPassword}
-            onChange={handleChange}
+            value={registerCredentials.confirmPassword}
+            onChange={handleRegisterChange}
             />
         <Button
             type="submit"
@@ -241,6 +253,8 @@ function LoginRegister(props) {
 }
 
 export default LoginRegister;
+
+
 
 
 
