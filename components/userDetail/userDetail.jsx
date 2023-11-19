f04b811945a1058bb9f9d99befeef8c930b69603
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2023-11-19 09:36:23 -0500

    userDetail final update

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Grid, TextField } from "@mui/material";
 import "./userDetail.css";
 import axios from "axios";
 
@@ -10,7 +10,12 @@ class UserDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      //set the state for the user
       user: undefined,
+      //set the state for the mostCommentedPhoto
+      mostCommentedPhoto: null,
+      //set the state for the most recentPhoto
+      mostRecentPost: null,
     };
   }
   componentDidMount() {
@@ -28,88 +33,61 @@ class UserDetail extends React.Component {
 
   handleUserChange(user_id) {
     axios.get("/user/" + user_id).then((response) => {
-      const new_user = response.data;
-      this.setState({
-        user: new_user,
-      });
+      const { user, mostRecentPost, mostCommentedPhoto } = response.data;
+      //set the state of the user, most recent photo posted, and most commented photo
+        this.setState({
+          user: user,
+          mostCommentedPhoto: mostCommentedPhoto,
+          mostRecentPost: mostRecentPost,
+        });
       const main_content =
-        "User Details for " + new_user.first_name + " " + new_user.last_name;
+        "User Details for " + user.first_name + " " + user.last_name;
       this.props.changeMainContent(main_content);
     });
   }
 
   render() {
-    return this.state.user ? (
-      <div>
-        <Box component="form" noValidate autoComplete="off">
+    const { user, mostRecentPost, mostCommentedPhoto } = this.state;
+
+  return user ? (
+    <Box component="form" noValidate autoComplete="off">
           <div>
-            <Button
-              variant="contained"
-              component="a"
-              href={"#/photos/" + this.state.user._id}
-            >
-              User Photos
-            </Button>
+            <TextField id="first_name" label="First Name" variant="outlined" disabled fullWidth margin="normal" value={this.state.user.first_name}/>
           </div>
           <div>
-            <TextField
-              id="first_name"
-              label="First Name"
-              variant="outlined"
-              disabled
-              fullWidth
-              margin="normal"
-              value={this.state.user.first_name}
-            />
+            <TextField id="last_name" label="Last Name" variant="outlined" disabled fullWidth margin="normal" value={this.state.user.last_name}/>
           </div>
           <div>
-            <TextField
-              id="last_name"
-              label="Last Name"
-              variant="outlined"
-              disabled
-              fullWidth
-              margin="normal"
-              value={this.state.user.last_name}
-            />
+            <TextField id="location" label="Location" variant="outlined" disabled fullWidth margin="normal" value={this.state.user.location}/>
           </div>
           <div>
-            <TextField
-              id="location"
-              label="Location"
-              variant="outlined"
-              disabled
-              fullWidth
-              margin="normal"
-              value={this.state.user.location}
-            />
+            <TextField id="description" label="Description" variant="outlined" multiline rows={4} disabled fullWidth margin="normal" value={this.state.user.description}/>
           </div>
           <div>
-            <TextField
-              id="description"
-              label="Description"
-              variant="outlined"
-              multiline
-              rows={4}
-              disabled
-              fullWidth
-              margin="normal"
-              value={this.state.user.description}
-            />
+            <TextField id="occupation" label="Occupation" variant="outlined" disabled fullWidth margin="normal" value={this.state.user.occupation}/>
           </div>
-          <div>
-            <TextField
-              id="occupation"
-              label="Occupation"
-              variant="outlined"
-              disabled
-              fullWidth
-              margin="normal"
-              value={this.state.user.occupation}
-            />
-          </div>
-        </Box>
-      </div>
+      <Grid container spacing={2}>
+        
+        {mostCommentedPhoto && (
+            <Grid item xs={6} md={8} style={{height: '40%', width: '50%', margin: '0 auto' }}>
+              <Button variant="contained" component="a" href={"#/photos/" + this.state.user._id} style={{ display: 'flex', alignItems: 'center', textAlign: 'left' }} >
+                <img src={"/images/" + mostCommentedPhoto.file_name} style={{ width: '20%', height: '30%', marginRight: '7px' }} />
+                <p>Number of Comments: {mostCommentedPhoto.comments.length}</p>
+              </Button>
+            </Grid>
+          )}
+
+
+          {mostRecentPost && (
+            <Grid item xs={6} md={8} style={{ height: '40%', width: '50%', margin: '0 auto' }}>
+              <Button variant="contained" component="a" href={"#/photos/" + this.state.user._id} style={{ display: 'flex', alignItems: 'center', textAlign: 'left' }}>
+                <img src={"/images/" + mostRecentPost.file_name} style={{ width: '20%', height: '30%', marginRight: '10px' }}/>
+                <p>Posted on: {new Date(mostRecentPost.date_time).toLocaleString()}</p>
+              </Button>
+            </Grid>
+        )}
+      </Grid>
+    </Box>
     ) : (
       <div />
     );
@@ -117,3 +95,4 @@ class UserDetail extends React.Component {
 }
 
 export default UserDetail;
+
